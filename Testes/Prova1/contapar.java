e7d70d180fe8d554fe737f030c5b9ff09b5018dd
Cauã Costa Alves <cauacostalves@gmail.com>
2024-09-19 16:28:53 -0300

    att

---
+++ Testes/Prova1/contapar.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class contapar{
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n);
+        sc.close();
+    }
+}
