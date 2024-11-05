5cb672ddeace0d952e868213e970b10bbf00a9f2
Cauã Costa Alves <cauacostalves@gmail.com>
2024-11-05 13:18:08 -0300

    att

--- Site de desafios/beecrowd/menordapilha.java
+++ Site de desafios/beecrowd/menordapilha.java
@@ -1,77 +1,42 @@
 import java.util.*;
 
-class Pilha{
-    int tam;
-    int []pilha;
-    int topo;
-
-    public Pilha(int t){
-        this.tam = t;
-        this.pilha = new int[tam];
-        this.topo = -1;
-    }
-
-    public boolean inserirpilha(int valor){
-        boolean flag = false;
-        if(topo > tam -1){
-            System.out.print("ERRO");
-            return flag;
+public class menorDaPilha {
+    public static int Min(Stack<Integer> pilha){
+        if (pilha.isEmpty()) {
+            throw new IllegalStateException("EMPTY\n");
         }
-        topo++;
-        pilha[topo] = valor; 
-        flag = true;
-        return flag;
-    }
-
-    public int removerpilha(){
-         if(topo < 0){
-            System.out.print("ERRO");
-            return -1;
-        }
-        int removido = pilha[topo];
-        topo--;
-        return removido;
-    }
-
-    public int menorvalorpilha(){
-        int menor = pilha[0];
-        for(int i = 1;i<=topo;i++){
-            if(menor > pilha[i]){
-                menor = pilha[i];
+        int menor = pilha.peek();
+        for (int elemento : pilha) {
+            if (elemento < menor) {
+                menor = elemento;
             }
         }
         return menor;
     }
-
-    public void imprimirpilha(){ 
-        for (int i = topo ; i >= 0;i--){
-            System.out.print( pilha[i] + " ");
-        }
-    }
-}
-
-public class menordapilha{
-
     public static void main(String[] Args){
         Scanner sc = new Scanner(System.in);
-        int acoes = sc.nextInt();
-        sc.nextLine();
-        Pilha pil = new Pilha(acoes);
-        while(acoes > 0 ){
-            String acao = sc.next();
-            if(acao.equals("PUSH")){ 
-                int num = sc.nextInt();
-                sc.nextLine();
-                System.out.println(pil.inserirpilha(num));
+        int N = sc.nextInt();
+        Stack<Integer> presentes = new Stack<>();
+        for(int i=0; i<N; i++){
+            String op = sc.next();
+            switch (op) {
+                case "POP":
+                    presentes.pop();
+                    break;
+                
+                case "PUSH":
+                    int input = sc.nextInt();
+                    presentes.push(input);
+                    break;
+                
+                case "MIN":
+                    System.out.println(Min(presentes));
+                    break;
+
+                default:
+                    break;
             }
-            else if(acao.equals("MIN")){
-                System.out.println(pil.menorvalorpilha());
-            }else if(acao.equals("POP")){
-                pil.removerpilha();
-            }
-            
-            acoes --;
         }
         sc.close();
-    }
+    }    
 }
