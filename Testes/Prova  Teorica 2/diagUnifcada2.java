cc746926534c2588f4eb6267c7d4a0a6970ab932
Cauã Costa Alves <91996777+cauacostaalves@users.noreply.github.com>
2024-11-12 14:55:18 -0300

    aatt

---
+++ Testes/Prova  Teorica 2/diagUnifcada2.java
@@ -0,0 +1,51 @@
+class Matriz{
+    CelulaMatriz inicio;
+    int linha, coluna;
+
+    CelulaDupla diagUnificada(){
+        return caminharDiagonal(inicio);
+    }
+
+    CelulaDupla caminharDiagonal(CelulaMatriz i){
+        CelulaDupla sentinela;
+        while(i != null){
+            MontaLista(sentinela, i.inicio, i.fim);
+            i = i.dir;
+            if(i != null){
+                i = i.inf;
+            }
+        }
+        return sentinela;
+    }
+
+    void MontaLista(CelulaDupla i, Celula incio, Celula fim){
+        for(Celula tmp = inicio; tmp != fim; tmp = tmp.prox){
+            inserirListaDupla(i, tmp.elemento);
+        }
+    }
+
+    void inserirListaDupla(CelulaDupla sentinela, int x){
+        CelulaDupla tmp = new CelulaDupla(x);
+        tmp = sentinela.prox;
+        sentinela.prox = tmp;
+        tmp.prox.ant = tmp;
+        tmp.ant = sentinela;
+    }
+}
+
+
+
+class CelulaMatriz{
+    CelulaMatriz esq, dis, inf , sup;
+    Celula incio,fim;
+}
+
+class Celula{
+    int elemento;
+    Celula prox;
+}
+
+class CelulaDupla{
+    int elemento;
+    CelulaDupla prox,ant;
+}
